544e1c77970a475f1685377940caf1dba2ca294b
win-bean <ksbdaniel@naver.com>
2023-07-17 22:38:57 +0900

    rsi sort strange data

---
+++ RSI_strategy.py
@@ -0,0 +1,129 @@
+import csv
+
+
+problem = []
+
+def parse_dataset():
+    dates = []
+    codes = []
+    names = []
+    volumes = []
+    closes = []
+
+    with open('train.csv', 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+
+        for row in csv_reader:
+            date = row[0]
+            code = row[1]
+            name = row[2]
+            volume = int(row[3])
+            close = int(row[7])
+
+            if volume == 0:
+                problem.append(code)
+            dates.append(date)
+            codes.append(code)
+            names.append(name)
+            volumes.append(volume)
+            closes.append(close)
+
+    return dates, codes, names, volumes, closes
+
+def calculate_rsi(prices, period):
+    # 가격 데이터를 기반으로 RSI 계산
+    changes = []
+    for i in range(1, len(prices)):
+        change = prices[i] - prices[i-1]
+        changes.append(change)
+    
+    gains = [change for change in changes if change >= 0]
+    losses = [-change for change in changes if change < 0]
+    
+    avg_gain = sum(gains) / period
+    avg_loss = sum(losses) / period
+    
+    for i in range(period, len(prices)):
+        change = changes[i-1]
+        if change >= 0:
+            avg_gain = (avg_gain * (period - 1) + change) / period
+            avg_loss = (avg_loss * (period - 1)) / period
+        else:
+            avg_gain = (avg_gain * (period - 1)) / period
+            avg_loss = (avg_loss * (period - 1) - change) / period
+    
+    if avg_loss != 0:
+        rsi = 100 - (100 / (1 + (avg_gain / avg_loss)))
+    else:
+        rsi = 100
+    
+    return rsi
+
+dates, codes, names, volumes, closes = parse_dataset()
+
+rsi_dict = {}
+period = 6
+
+for code in set(codes):
+    index_list = [i for i, x in enumerate(codes) if x == code]
+    code_closes = [closes[i] for i in index_list]
+    rsi = calculate_rsi(code_closes, period)
+    rsi_dict[code] = rsi
+
+sorted_rsi = sorted(rsi_dict.items(), key=lambda x: x[1], reverse=True)
+
+for rank, item in enumerate(sorted_rsi, start=1):
+    code = item[0]
+
+with open('baseline_submission.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['종목코드', '순위'])
+
+    for rank, item in enumerate(sorted_rsi, start=1):
+        code = item[0]
+        writer.writerow([code, rank])
+
+
+#이상한 종목들 중 상위200 하위 200에 들어가는 종목들 분류
+
+data = {}
+with open('baseline_submission.csv', newline='') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        code = row[0]
+        rank = int(row[1])
+        data[code] = rank
+    
+i=1
+s=1
+print(set(problem))
+
+for code in set(problem):
+    if code in data:
+        rank = data[code]
+        test1 = 200+i
+        test2 = 1800-s
+        before = next(key for key, value in data.items() if value == rank)
+        if rank <= 200:
+            after = next(key for key, value in data.items() if value == test1)
+            data[before], data[after] = data[after], data[before]
+            i += 1
+            
+        elif rank >= 1800:
+            after = next(key for key, value in data.items() if value == test2)
+            data[before], data[after] = data[after], data[before]
+            s += 1
+            
+sorted_data = dict(sorted(data.items(), key=lambda x: x[1]))
+
+for i, (code, rank) in enumerate(sorted_data.items(), start=1):
+    sorted_data[code] = i
+
+with open("adjusted_submission.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['종목코드', '순위'])
+
+    for code, rank in sorted_data.items():
+        writer.writerow([code, rank])
