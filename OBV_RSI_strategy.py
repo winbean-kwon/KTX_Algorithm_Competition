c21ef2e08b69251cac74657b154bbd69cd1416f9
win-bean <ksbdaniel@naver.com>
2023-07-18 22:22:08 +0900

    use both strategy(OBV & RSI) and some modify

--- OBV_RSI_strategy.py
+++ OBV_RSI_strategy.py
@@ -3,17 +3,16 @@ import csv
 
 problem = []
 
-def parse_dataset(filename):
+def parse_dataset():
     dates = []
     codes = []
     names = []
     volumes = []
     closes = []
 
-
     with open('train.csv', 'r') as file:
         csv_reader = csv.reader(file)
-        next(csv_reader)  # 헤더 라인 건너뛰기
+        next(csv_reader)
 
         for row in csv_reader:
             date = row[0]
@@ -21,7 +20,7 @@ def parse_dataset(filename):
             name = row[2]
             volume = int(row[3])
             close = int(row[7])
-            
+
             if volume == 0:
                 problem.append(code)
             dates.append(date)
@@ -31,6 +30,35 @@ def parse_dataset(filename):
             closes.append(close)
 
     return dates, codes, names, volumes, closes
+
+def calculate_rsi(prices, period):
+    # 가격 데이터를 기반으로 RSI 계산
+    changes = []
+    for i in range(1, len(prices)):
+        change = prices[i] - prices[i-1]
+        changes.append(change)
+    
+    gains = [change for change in changes if change >= 0]
+    losses = [-change for change in changes if change < 0]
+    
+    avg_gain = sum(gains) / period
+    avg_loss = sum(losses) / period
+    
+    for i in range(period, len(prices)):
+        change = changes[i-1]
+        if change >= 0:
+            avg_gain = (avg_gain * (period - 1) + change) / period
+            avg_loss = (avg_loss * (period - 1)) / period
+        else:
+            avg_gain = (avg_gain * (period - 1)) / period
+            avg_loss = (avg_loss * (period - 1) - change) / period
+    
+    if avg_loss != 0:
+        rsi = 100 - (100 / (1 + (avg_gain / avg_loss)))
+    else:
+        rsi = 100
+    
+    return rsi
 
 def calculate_obv(volumes, closes):
     obv = [0]  # 초기 OBV 값을 0으로 설정
@@ -51,36 +79,42 @@ def calculate_obv(volumes, closes):
 
 dates, codes, names, volumes, closes = parse_dataset()
 
-obv_dict = {}  # 종목별 OBV 값을 저장할 딕셔너리
+rsi_dict = {}
+obv_dict = {}
+period = 6
 
-for code in set(codes):  # 종목 코드를 기준으로 반복
-    index_list = [i for i, x in enumerate(codes) if x == code]  # 종목 코드에 해당하는 인덱스 리스트 추출
-    code_volumes = [volumes[i] for i in index_list]  # 종목 코드에 해당하는 거래량 리스트 추출
-    code_closes = [closes[i] for i in index_list]  # 종목 코드에 해당하는 종가 리스트 추출
+# Calculate RSI and OBV for each stock
+for code in set(codes):
+    index_list = [i for i, x in enumerate(codes) if x == code]
+    code_closes = [closes[i] for i in index_list]
+    rsi = calculate_rsi(code_closes, period)
+    rsi_dict[code] = rsi
+
+    code_volumes = [volumes[i] for i in index_list]
+    code_closes = [closes[i] for i in index_list]
     obv = calculate_obv(code_volumes, code_closes)
-    obv_dict[code] = obv[-1]  # 최신 OBV 값을 딕셔너리에 저장
-    
+    obv_dict[code] = obv[-1]
 
-# OBV 값을 기준으로 종목을 상승 가능성이 높은 순서로 정렬
+sorted_rsi = sorted(rsi_dict.items(), key=lambda x: x[1], reverse=True)
 sorted_obv = sorted(obv_dict.items(), key=lambda x: x[1], reverse=True)
 
-for rank, item in enumerate(sorted_obv, start=1):
-    code = item[0]
-    
+#Combine the rankings
+combined_ranking = {}
+for rank, (code, _) in enumerate(sorted_rsi):
+    combined_ranking[code] = rank
 
-# 결과를 baseline_submission.csv 파일에 작성
+for rank, (code, _) in enumerate(sorted_obv):
+    combined_ranking[code] += rank
+
+# Sort the stocks based on the combined ranking
+sorted_combined = sorted(combined_ranking.items(), key=lambda x: x[1])
+
 with open('baseline_submission.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['종목코드', '순위'])
 
-    for rank, item in enumerate(sorted_obv, start=1):
-        code = item[0]
+    for rank, (code, _) in enumerate(sorted_combined, start=1):
         writer.writerow([code, rank])
-    
-
-
-
-#이상한 종목들 중 상위200 하위 200에 들어가는 종목들 분류
 
 data = {}
 with open('baseline_submission.csv', newline='') as file:
@@ -122,7 +156,3 @@ with open("adjusted_submission.csv", 'w', newline='') as file:
 
     for code, rank in sorted_data.items():
         writer.writerow([code, rank])
-
-
-# 6개월치 데이터로 돌려보는 것도 방법?
-# 다른 지표 찾아보기 MACD?
