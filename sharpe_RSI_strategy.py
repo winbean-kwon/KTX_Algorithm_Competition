aa475466f489cc74391bd05a28e2223edd441ba3
win-bean <ksbdaniel@naver.com>
2023-07-29 21:29:20 +0900

    sharpe check for competition almost same

--- sharpe_RSI_strategy.py
+++ sharpe_RSI_strategy.py
@@ -2,6 +2,10 @@ import csv
 import numpy as np
 import pandas as pd
 
+
+# 모든 행과 열을 표시하도록 출력 옵션 설정
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 problem = []
 
 def parse_dataset():
@@ -35,7 +39,7 @@ def parse_dataset():
 def calculate_rsi(prices, period):
     # 가격 데이터를 기반으로 RSI 계산
     changes = []
-    for i in range(1, len(prices)):
+    for i in range(1, 16):
         change = prices[i] - prices[i-1]
         changes.append(change)
     
@@ -45,7 +49,7 @@ def calculate_rsi(prices, period):
     avg_gain = sum(gains) / period
     avg_loss = sum(losses) / period
     
-    for i in range(period, len(prices)):
+    for i in range(period, 16):
         change = changes[i-1]
         if change >= 0:
             avg_gain = (avg_gain * (period - 1) + change) / period
@@ -62,10 +66,16 @@ def calculate_rsi(prices, period):
     return rsi
 
 def calculate_sharpe_ratio(returns, risk_free_rate=0.035):
+    alpha = 0.2
     # 샤프 지수를 계산합니다.
     excess_returns = returns - risk_free_rate
-    mean_excess_return = np.mean(excess_returns)
-    std_excess_return = np.std(excess_returns)
+    data = np.array(excess_returns)
+    weighted_average = np.zeros_like(data)
+    weighted_average[0] = data[0]
+    for t in range(1, 16):
+        weighted_average[t] = (1 - alpha) * data[t] + alpha * weighted_average[t - 1]
+    mean_excess_return = np.mean(weighted_average)
+    std_excess_return = np.std(weighted_average)
     sharpe_ratio = mean_excess_return / std_excess_return
     
     return sharpe_ratio
@@ -74,19 +84,21 @@ dates, codes, names, volumes, closes = parse_dataset()
 
 sharpe_dict = {}
 rsi_dict = {}
-period=10
+period = 15
+sorted_codes = []
+[sorted_codes.append(code) for code in codes if code not in sorted_codes]
 
-for code in set(codes):
-    index_list = [i for i, x in enumerate(codes) if x == code]
+for code in sorted_codes:
+    
+    index_list = [i for i, x in enumerate(codes) if x == code][:-15]
     code_closes = [closes[i] for i in index_list]
     # 종가를 기반으로 수익률 계산
     returns = np.diff(code_closes) / code_closes[:-1]
     returns = np.concatenate([[0], returns])  # returns 배열의 길이를 원래 데이터와 동일하게 만듭니다.
+    
     sharpe = calculate_sharpe_ratio(returns, risk_free_rate=0.035)
     sharpe_dict[code] = sharpe
 
-    index_list = [i for i, x in enumerate(codes) if x == code]
-    code_closes = [closes[i] for i in index_list]
     rsi = calculate_rsi(code_closes, period)
     rsi_dict[code] = rsi
 
@@ -100,18 +112,20 @@ df_rsi = pd.DataFrame(sorted_rsi, columns=['종목코드', '순위_rsi'])
 # 두 데이터프레임을 '종목코드'를 기준으로 합치기
 merged_data = pd.merge(df_sharpe, df_rsi, on='종목코드')
 
+# print(merged_data.head(200))
+# print(merged_data.tail(200))
+
+
 # 상관관계 계산하기
 correlation = merged_data['순위_sharpe'].corr(merged_data['순위_rsi'])
-
-print("RSI와 샤프지수 전략의 상관관계:", correlation)
 
 #Combine the rankings
 combined_ranking = {}
 for rank, (code, _) in enumerate(sorted_rsi):
-    combined_ranking[code] = rank
+    combined_ranking[code] = rank + combined_ranking.get(code, 0)
 
 for rank, (code, _) in enumerate(sorted_sharpe):
-    combined_ranking[code] += rank
+    combined_ranking[code] = rank + combined_ranking.get(code, 0)
 
 # Sort the stocks based on the combined ranking
 sorted_combined = sorted(combined_ranking.items(), key=lambda x: x[1])
@@ -134,25 +148,25 @@ with open('baseline_submission.csv', newline='') as file:
         rank = int(row[1])
         data[code] = rank
     
-i=1
-s=1
-print(set(problem))
+i = 1
+s = 1
 
-for code in set(problem):
+for code in sorted(problem):
     if code in data:
         rank = data[code]
-        test1 = 200+i
-        test2 = 1800-s
+        test1 = 200 + i
+        test2 = 1801 - s
         before = next(key for key, value in data.items() if value == rank)
         if rank <= 200:
             after = next(key for key, value in data.items() if value == test1)
             data[before], data[after] = data[after], data[before]
             i += 1
             
-        elif rank >= 1800:
+        elif rank >= 1801:
             after = next(key for key, value in data.items() if value == test2)
             data[before], data[after] = data[after], data[before]
             s += 1
+
             
 sorted_data = dict(sorted(data.items(), key=lambda x: x[1]))
 
@@ -166,3 +180,78 @@ with open("adjusted_submission.csv", 'w', newline='') as file:
     for code, rank in sorted_data.items():
         writer.writerow([code, rank])
         
+# 분류된 주식에 따라 마지막 15일 데이터로 샤프지수 계산
+returns = {}
+final_buy_returns = 0
+final_sell_returns = 0
+
+for code in sorted_codes:
+    rank = sorted_data.get(code)
+
+    if 1 <= rank <= 200 or 1801 <= rank <= 2000:
+    
+        # 마지막 15일 동안의 주식 가격 데이터 가져오기
+        index_list = [i for i, x in enumerate(codes) if x == code][-16:]
+        code_closes = [closes[i] for i in index_list]
+
+        # 최종 매수, 공매도 수익률
+        if rank <= 200:
+            final_buy_returns += ((code_closes[-1]-code_closes[1]) / code_closes[1])
+   
+        if rank >= 1801:
+            final_sell_returns += ((code_closes[-1]-code_closes[1]) / code_closes[1]) * -1
+
+        # 일간 매수 수익률
+        if rank >= 1801:
+            daily_returns = [((code_closes[i] - code_closes[i - 1]) / code_closes[i - 1]) * -1 for i in range(1, 16)]
+        else:
+            daily_returns = [(code_closes[i] - code_closes[i - 1]) / code_closes[i - 1] for i in range(1, 16)]
+
+        returns[code] = daily_returns
+
+n_values = range(2, 16)  # n=2부터 n=15까지
+
+# 각 n에 해당하는 일간 수익률을 저장할 리스트 초기화
+avg_n_day_returns = [0] * len(n_values)
+
+# 종목별로 2일부터 15일까지의 일간 수익률을 더해줍니다.
+for code, daily_returns in returns.items():
+    for i, n in enumerate(n_values):
+        if len(daily_returns) >= n:
+            avg_n_day_returns[i] += daily_returns[n-1]
+
+# 종목별로 2일부터 15일까지의 일간 수익률 평균을 구합니다.
+total_num_codes = len(returns)
+avg_n_day_returns = [total_return / 400 for total_return in avg_n_day_returns]
+
+# 전체 종목에 대한 일간 수익률을 리스트로 변환
+all_daily_returns = [daily_returns for daily_returns_list in returns.values() for daily_returns in daily_returns_list]
+
+# 연율화된 일간 수익률의 평균
+avg_daily_return = np.mean(all_daily_returns) * 250
+
+# n=2에서 n=15까지의 연율화된 n 번째 매매일의 일간 수익률의 평균을 구하고, 이들의 차이를 제곱하여 합산
+sum_diff_squared = 0
+        
+for i in range(0,14):
+
+    avg_n_day_return = avg_n_day_returns[i] * 250
+    diff_squared = (avg_n_day_return - avg_daily_return) ** 2
+    sum_diff_squared += diff_squared
+
+# 변동성 계산
+volatility = np.sqrt(sum_diff_squared / 13)
+
+# 마지막 15일 동안의 연율화된 총 수익률 계산
+avg_total_returns = ((final_buy_returns + final_sell_returns) / 400) * 250 / 15
+
+# 샤프지수 계산
+risk_free_rate = 0.035  # 연율화된 무위험 수익률 (3.5%)
+sharpe_ratio = (avg_total_returns - risk_free_rate) / volatility
+
+# print(cumulative_returns)
+# print(long_returns_sum)
+# print(short_returns_sum)
+# print(avg_total_returns)
+# print(volatility)
+print("마지막 15일 동안의 샤프지수:", sharpe_ratio)
